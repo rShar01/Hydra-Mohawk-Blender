d4a12989c12ab874b774cce76799d5fda8d50bc8
Ryan Shar <ryan.shar01@gmail.com>
2024-11-16 16:21:28 -0500

    foward intermediates

--- test.py
+++ test.py
@@ -1,7 +1,7 @@
 import warnings
 import torch
 import torchvision.transforms as T
-
+import timm
 
 from deit.models import deit_tiny_distilled_patch16_224
 from datasets import load_dataset
@@ -34,19 +34,20 @@ class ImageNetDataset(Dataset):
 
 if __name__ == "__main__":
     warnings.filterwarnings("ignore")
-    model = deit_tiny_distilled_patch16_224(pretrained=True)
+    # model = deit_tiny_distilled_patch16_224(pretrained=True)
+    model = timm.create_model('vit_tiny_patch16_224', pretrained=True)
     print("ok")
     
     imagenet_train = load_dataset('Maysee/tiny-imagenet', split='train')
     imagenet_val_combined = load_dataset('Maysee/tiny-imagenet', split='valid')
 
-    # imagenet_val_test = imagenet_val_combined.train_test_split(test_size=0.5, stratify_by_column='label')
-    # imagenet_val = imagenet_val_test['train']
-    # imagenet_test = imagenet_val_test['test']
-    imagenet_train = load_dataset('ILSVRC/imagenet-1k', split='train', streaming=True, trust_remote_code=True)
-    imagenet_train = imagenet_train.shuffle(seed=42, buffer_size=1000)
+    imagenet_val_test = imagenet_val_combined.train_test_split(test_size=0.5, stratify_by_column='label')
+    imagenet_val = imagenet_val_test['train']
+    imagenet_test = imagenet_val_test['test']
+    # imagenet_train = load_dataset('ILSVRC/imagenet-1k', split='train', streaming=True, trust_remote_code=True)
+    # imagenet_train = imagenet_train.shuffle(seed=42, buffer_size=1000)
 
-
+    # Facebook transform
     transform = T.Compose([
         T.Resize(256, interpolation=3),
         T.CenterCrop(224),
@@ -54,16 +55,27 @@ if __name__ == "__main__":
         T.Normalize(IMAGENET_DEFAULT_MEAN, IMAGENET_DEFAULT_STD),
     ])
 
-    # train_dataset = ImageNetDataset(imagenet_train, transform=transform)
-    # train_dataloader = DataLoader(train_dataset, batch_size=32, shuffle=True)
+    # google transform
+    transform = T.Compose([
+        T.Resize((224, 224)),
+        T.ToTensor(),
+        T.Lambda(lambda x: x.repeat(3, 1, 1) if x.size(0) == 1 else x), # apparently some images are not RGB
+        T.Normalize(mean=[0.485, 0.456, 0.406], std=[0.229, 0.224, 0.225])
+    ])
 
-    sample_data = next(iter(imagenet_train))
-    sample_X = transform(sample_data['image'])
+    train_dataset = ImageNetDataset(imagenet_train, transform=transform)
+    train_dataloader = DataLoader(train_dataset, batch_size=32, shuffle=True)
+
+    sample_X, sample_y = next(iter(imagenet_train))
     print(sample_X)
     print(sample_X.size())
-    x, x_dist = model(sample_X)
-    print(x)
-    print(x_dist)
+    # x, x_dist = model(sample_X)
+    x_emb = model.patch_embed(sample_X)
+    print(x_emb)
+    print(x_emb.size())
 
-    print(x.size())
-    print(x_dist.size())
+    inter = model.forward_intermediates(x_emb)
+    print(inter)
+    print(inter.size())
+
+
