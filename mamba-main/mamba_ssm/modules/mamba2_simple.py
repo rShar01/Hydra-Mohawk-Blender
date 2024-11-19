4cd8bea61dae952e1c17d8384bc0e8886101f9ab
Ryan Shar <ryan.shar01@gmail.com>
2024-11-19 14:14:05 -0500

    added mamba

--- mamba-main/mamba_ssm/modules/mamba2_simple.py
+++ mamba-main/mamba_ssm/modules/mamba2_simple.py
@@ -1,5 +1,4 @@
-# Copyright (c) 2024, Sukjun Hwang, Aakash Lahoti, Ratish Puduppully, Tri Dao, Albert Gu.
-# Base code from https://github.com/state-spaces/mamba/blob/main/mamba_ssm/modules/mamba2_simple.py
+# Copyright (c) 2024, Tri Dao, Albert Gu.
 
 import math
 import torch
@@ -9,26 +8,30 @@ import torch.nn.functional as F
 from einops import rearrange, repeat
 
 try:
-    from layernorm_gated import RMSNorm as RMSNormGated
+    from causal_conv1d import causal_conv1d_fn
 except ImportError:
-    RMSNormGated = None
+    causal_conv1d_fn = None
 
-from ssd_combined import mamba_chunk_scan_combined
+try:
+    from mamba_ssm.ops.triton.layernorm_gated import RMSNorm as RMSNormGated, LayerNorm
+except ImportError:
+    RMSNormGated, LayerNorm = None, None
 
-from .ops import hydra_split_conv1d_scan_combined
+from mamba_ssm.ops.triton.ssd_combined import mamba_chunk_scan_combined
+from mamba_ssm.ops.triton.ssd_combined import mamba_split_conv1d_scan_combined
 
 
-class Hydra(nn.Module):
-
+class Mamba2Simple(nn.Module):
     def __init__(
         self,
         d_model,
         d_state=64,
-        d_conv=7,
+        d_conv=4,
         conv_init=None,
         expand=2,
-        headdim=64,
+        headdim=128,
         ngroups=1,
+        A_init_range=(1, 16),
         dt_min=0.001,
         dt_max=0.1,
         dt_init_floor=1e-4,
@@ -39,7 +42,7 @@ class Hydra(nn.Module):
         conv_bias=True,
         # Fused kernel and sharding options
         chunk_size=256,
-        use_mem_eff_path=False,
+        use_mem_eff_path=True,
         layer_idx=None,  # Absorb kwarg for general module
         device=None,
         dtype=None,
@@ -64,17 +67,17 @@ class Hydra(nn.Module):
         self.layer_idx = layer_idx
 
         # Order: [z, x, B, C, dt]
-        d_in_proj = 2 * self.d_inner + 2 * (2 * self.ngroups * self.d_state) + 2 * self.nheads
+        d_in_proj = 2 * self.d_inner + 2 * self.ngroups * self.d_state + self.nheads
         self.in_proj = nn.Linear(self.d_model, d_in_proj, bias=bias, **factory_kwargs)
 
-        conv_dim = self.d_inner + 2 * (2 * self.ngroups * self.d_state)
+        conv_dim = self.d_inner + 2 * self.ngroups * self.d_state
         self.conv1d = nn.Conv1d(
             in_channels=conv_dim,
             out_channels=conv_dim,
             bias=conv_bias,
             kernel_size=d_conv,
             groups=conv_dim,
-            padding=d_conv // 2,
+            padding=d_conv - 1,
             **factory_kwargs,
         )
         if self.conv_init is not None:
@@ -101,7 +104,8 @@ class Hydra(nn.Module):
         self.dt_bias._no_weight_decay = True
 
         # A parameter
-        A = torch.ones(self.nheads, dtype=torch.float32, device=device)
+        assert A_init_range[0] > 0 and A_init_range[1] >= A_init_range[0]
+        A = torch.empty(self.nheads, dtype=torch.float32, device=device).uniform_(*A_init_range)
         A_log = torch.log(A).to(dtype=dtype)
         self.A_log = nn.Parameter(A_log)
         # self.register_buffer("A_log", torch.zeros(self.nheads, dtype=torch.float32, device=device), persistent=True)
@@ -110,11 +114,10 @@ class Hydra(nn.Module):
         # D "skip" parameter
         self.D = nn.Parameter(torch.ones(self.nheads, device=device))
         self.D._no_weight_decay = True
-        self.fc_D = nn.Linear(self.d_inner, self.nheads, bias=False, **factory_kwargs)
 
         # Extra normalization layer right before output projection
         assert RMSNormGated is not None
-        self.norm = RMSNormGated(self.d_inner, eps=1e-5, norm_before_gate=True, **factory_kwargs)
+        self.norm = RMSNormGated(self.d_inner, eps=1e-5, norm_before_gate=False, **factory_kwargs)
 
         self.out_proj = nn.Linear(self.d_inner, self.d_model, bias=bias, **factory_kwargs)
 
@@ -126,83 +129,72 @@ class Hydra(nn.Module):
         batch, seqlen, dim = u.shape
 
         zxbcdt = self.in_proj(u)  # (B, L, d_in_proj)
-        A = -torch.exp(self.A_log.float())  # (nheads) or (d_inner, d_state)
-        initial_states = repeat(self.init_states, "... -> b ...", b=2*batch) if self.learnable_init_states else None
+        A = -torch.exp(self.A_log)  # (nheads) or (d_inner, d_state)
+        initial_states=repeat(self.init_states, "... -> b ...", b=batch) if self.learnable_init_states else None
         dt_limit_kwargs = {} if self.dt_limit == (0.0, float("inf")) else dict(dt_limit=self.dt_limit)
 
         if self.use_mem_eff_path:
-            return hydra_split_conv1d_scan_combined(
+            # Fully fused path
+            out = mamba_split_conv1d_scan_combined(
                 zxbcdt,
-                self.conv1d.weight,
+                rearrange(self.conv1d.weight, "d 1 w -> d w"),
                 self.conv1d.bias,
-                self.dt_limit,
                 self.dt_bias,
                 A,
-                self.fc_D.weight,
-                self.D,
-                self.norm.weight,
-                self.norm.eps,
-                self.out_proj.weight,
-                self.out_proj.bias,
-                self.chunk_size,
-                initial_states,
-                seq_idx,
-                self.d_inner,
-                self.d_state,
-                self.headdim,
-                self.ngroups,
+                D=self.D,
+                chunk_size=self.chunk_size,
+                seq_idx=seq_idx,
+                activation=self.activation,
+                rmsnorm_weight=self.norm.weight,
+                rmsnorm_eps=self.norm.eps,
+                outproj_weight=self.out_proj.weight,
+                outproj_bias=self.out_proj.bias,
+                headdim=self.headdim,
+                ngroups=self.ngroups,
+                norm_before_gate=False,
+                initial_states=initial_states,
+                **dt_limit_kwargs,
             )
+        else:
+            z, xBC, dt = torch.split(
+                zxbcdt, [self.d_inner, self.d_inner + 2 * self.ngroups * self.d_state, self.nheads], dim=-1
+            )
+            dt = F.softplus(dt + self.dt_bias)  # (B, L, nheads)
+            assert self.activation in ["silu", "swish"]
 
-        z, xBC, dt = torch.split(
-            zxbcdt,
-            [self.d_inner, self.d_inner + 2 * (2 * self.ngroups * self.d_state), 2 * self.nheads],
-            dim=-1
-        )
+            # 1D Convolution
+            if causal_conv1d_fn is None or self.activation not in ["silu", "swish"]:
+                xBC = self.act(
+                    self.conv1d(xBC.transpose(1, 2)).transpose(1, 2)
+                )  # (B, L, self.d_inner + 2 * ngroups * d_state)
+                xBC = xBC[:, :seqlen, :]
+            else:
+                xBC = causal_conv1d_fn(
+                    x=xBC.transpose(1, 2),
+                    weight=rearrange(self.conv1d.weight, "d 1 w -> d w"),
+                    bias=self.conv1d.bias,
+                    activation=self.activation,
+                ).transpose(1, 2)
 
-        dt = torch.cat((dt[:, :, :self.nheads], torch.flip(dt[:, :, self.nheads:], (1,))), dim=0)
-        dt = F.softplus(dt + self.dt_bias)  # (2 * B, L, nheads)
-        assert self.activation in ["silu", "swish"]
+            # Split into 3 main branches: X, B, C
+            # These correspond to V, K, Q respectively in the SSM/attention duality
+            x, B, C = torch.split(xBC, [self.d_inner, self.ngroups * self.d_state, self.ngroups * self.d_state], dim=-1)
+            y = mamba_chunk_scan_combined(
+                rearrange(x, "b l (h p) -> b l h p", p=self.headdim),
+                dt,
+                A,
+                rearrange(B, "b l (g n) -> b l g n", g=self.ngroups),
+                rearrange(C, "b l (g n) -> b l g n", g=self.ngroups),
+                chunk_size=self.chunk_size,
+                D=self.D,
+                z=None,
+                seq_idx=seq_idx,
+                initial_states=initial_states,
+                **dt_limit_kwargs,
+            )
+            y = rearrange(y, "b l h p -> b l (h p)")
 
-        # 1D Convolution
-        xBC = self.act(
-            self.conv1d(xBC.transpose(1, 2)).transpose(1, 2)
-        )  # (B, L, self.d_inner + 2 * (2 * ngroups * d_state))
-
-        # Split into 3 main branches: X, B, C
-        # These correspond to V, K, Q respectively in the SSM/attention duality
-        x, BC = torch.split(xBC, [self.d_inner, 2 * (2 * self.ngroups * self.d_state)], dim=-1)
-        x_og = x
-        x = torch.cat((x, torch.flip(x, (1,))), dim=0)
-        BC = torch.cat(
-            (BC[:, :, :2 * self.ngroups * self.d_state],
-             torch.flip(BC[:, :, 2 * self.ngroups * self.d_state:], (1,))),
-            dim=0
-        )
-        B, C = torch.split(BC, [self.ngroups * self.d_state, self.ngroups * self.d_state], dim=-1)
-
-        y = mamba_chunk_scan_combined(
-            rearrange(x, "b l (h p) -> b l h p", p=self.headdim),
-            dt,
-            A,
-            rearrange(B, "b l (g n) -> b l g n", g=self.ngroups),
-            rearrange(C, "b l (g n) -> b l g n", g=self.ngroups),
-            chunk_size=self.chunk_size,
-            D=None,
-            z=None,
-            seq_idx=seq_idx,
-            initial_states=initial_states,
-            **dt_limit_kwargs,
-        )
-        y = rearrange(y, "b l h p -> b l (h p)")
-        y = torch.roll(y, shifts=1, dims=1)
-        y[:, 0, :] = 0.0
-        y_fw, y_bw = y[:batch], torch.flip(y[batch:], (1,))
-        y = y_fw + y_bw + x_og * repeat(
-            F.linear(x_og, self.fc_D.weight, bias=self.D), "b l h -> b l (h p)", p=self.headdim
-        )
-
-        # Multiply "gate" branch and apply extra normalization layer
-        y = self.norm(y, z)
-        out = self.out_proj(y)
-
+            # Multiply "gate" branch and apply extra normalization layer
+            y = self.norm(y, z)
+            out = self.out_proj(y)
         return out
