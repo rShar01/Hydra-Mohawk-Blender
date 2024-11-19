206f3967d3b1eb1ad02b46aefd7a3a1a2bf1788f
Ryan Shar <ryan.shar01@gmail.com>
2024-11-19 14:32:19 -0500

    more copy paste

--- hydra_main/hydra/modules/mamba_main/mamba_ssm/ops/triton/ssd_combined.py
+++ hydra_main/hydra/modules/mamba_main/mamba_ssm/ops/triton/ssd_combined.py
@@ -11,7 +11,27 @@ from packaging import version
 import torch
 import torch.nn.functional as F
 from torch import Tensor
-from mamba_ssm.utils.torch import custom_bwd, custom_fwd
+
+# from mamba_ssm.utils.torch import custom_bwd, custom_fwd
+def custom_amp_decorator(dec: Callable, cuda_amp_deprecated: bool):
+    def decorator(*args, **kwargs):
+        if cuda_amp_deprecated:
+            kwargs["device_type"] = "cuda"
+        return dec(*args, **kwargs)
+    return decorator
+
+
+if hasattr(torch.amp, "custom_fwd"): # type: ignore[attr-defined]
+    deprecated = True
+    from torch.amp import custom_fwd, custom_bwd # type: ignore[attr-defined]
+else:
+    deprecated = False
+    from torch.cuda.amp import custom_fwd, custom_bwd
+
+custom_fwd = custom_amp_decorator(custom_fwd, deprecated)
+custom_bwd = custom_amp_decorator(custom_bwd, deprecated)
+
+
 
 import triton
 import triton.language as tl
