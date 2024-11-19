6316b84b33554845cbfb4f2fccc3a2c0adc00c4e
Ryan Shar <ryan.shar01@gmail.com>
2024-11-19 14:46:15 -0500

    more manual replace

--- hydra_main/hydra/modules/mamba_main/mamba_ssm/ops/selective_scan_interface.py
+++ hydra_main/hydra/modules/mamba_main/mamba_ssm/ops/selective_scan_interface.py
@@ -2,7 +2,28 @@
 
 import torch
 import torch.nn.functional as F
-from mamba_ssm.utils.torch import custom_bwd, custom_fwd
+# from mamba_ssm.utils.torch import custom_bwd, custom_fwd
+
+from typing import Callable
+
+def custom_amp_decorator(dec: Callable, cuda_amp_deprecated: bool):
+    def decorator(*args, **kwargs):
+        if cuda_amp_deprecated:
+            kwargs["device_type"] = "cuda"
+        return dec(*args, **kwargs)
+    return decorator
+
+
+if hasattr(torch.amp, "custom_fwd"): # type: ignore[attr-defined]
+    deprecated = True
+    from torch.amp import custom_fwd, custom_bwd # type: ignore[attr-defined]
+else:
+    deprecated = False
+    from torch.cuda.amp import custom_fwd, custom_bwd
+
+custom_fwd = custom_amp_decorator(custom_fwd, deprecated)
+custom_bwd = custom_amp_decorator(custom_bwd, deprecated)
+
 
 from einops import rearrange, repeat
 
